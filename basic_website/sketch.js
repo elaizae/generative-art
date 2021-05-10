501fc9f8b99519a4d81da8426280c036a155796d
Elaiza Elizabeth <43956037+elaizae@users.noreply.github.com>
2021-05-10 16:46:29 +0200

    Update sketch.js

--- basic_website/sketch.js
+++ basic_website/sketch.js
@@ -172,8 +172,8 @@ drawRandomShape("rectangle");
 function mousePressed(){
   const dx = constrain(rotationY, -3,3);
   const dy = constrain(rotationX, -3,3);
-  cx += dx*2;
-  cy += dy*2; 
+  cx += dx*10;
+  cy += dy*10; 
   cx = constrain(cx, 0, width);
   cy = constrain(cy, 0, height);
   fill(255, 204, 0);
