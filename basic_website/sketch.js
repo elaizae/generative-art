5a83cc0700fdca68a67cac25d9c0f6c8b5be4899
Elaiza Elizabeth <43956037+elaizae@users.noreply.github.com>
2021-05-10 16:41:02 +0200

    Update sketch.js

--- basic_website/sketch.js
+++ basic_website/sketch.js
@@ -168,20 +168,18 @@ noLoop();
 //loop();
 
  }
- 
-function move(){
-  loop();
+ loop();
+function mousePressed(){
   const dx = constrain(rotationY, -3,3);
   const dy = constrain(rotationX, -3,3);
-  cx += dx*5;
-  cy += dy*5; 
+  cx += dx*2;
+  cy += dy*2; 
   cx = constrain(cx, 0, width);
   cy = constrain(cy, 0, height);
   fill(255, 204, 0);
  
-  ellipse(cx, cy , 45 , 45);
+  ellipse(cx, cy , 25 , 25);
 
-  move 
 }
 
 
