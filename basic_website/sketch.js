b862362d5ab0f8865a8680dc4084ebc0d0f1eef4
Elaiza Elizabeth <43956037+elaizae@users.noreply.github.com>
2021-05-16 18:33:01 +0200

    Update sketch.js

--- basic_website/sketch.js
+++ basic_website/sketch.js
@@ -170,12 +170,12 @@ let stop = '';
  
  // only make sure my circle moves
  loop();
- var Youcircle = ellipse(cx, cy , 15 , 15);
+
 
  
 
 function deviceMoved(){
-  
+  var Youcircle = ellipse(cx, cy , 15 , 15);
 
   const dx = constrain(rotationY, -3,3);
   const dy = constrain(rotationX, -3,3);
@@ -191,11 +191,7 @@ function deviceMoved(){
   
 }
 
-function mouseClicked(){
 
-  fill(someColors1[randomColor]);
-  
-}
 
 
 
