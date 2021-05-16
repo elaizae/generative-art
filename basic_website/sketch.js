6ed5d598b1f922701b6c1b669567df008c886dfe
Elaiza Elizabeth <43956037+elaizae@users.noreply.github.com>
2021-05-16 20:59:14 +0200

    Update sketch.js

--- basic_website/sketch.js
+++ basic_website/sketch.js
@@ -177,8 +177,8 @@ let stop = '';
 function deviceMoved(){
   var Youcircle = rect(cx, cy , 15);
  // dont go off the screen
-  const dx = constrain(rotationY, -2,2);
-  const dy = constrain(rotationX, -2,2);
+  const dx = constrain(rotationY, -0,0);
+  const dy = constrain(rotationX, -0,0);
   // circle speed 
   cx += dx*2;
   cy += dy*2; 
