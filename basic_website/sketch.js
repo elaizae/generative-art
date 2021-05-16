08f943c62b8f09daf3ee88a4cd0f2227e6d303ff
Elaiza Elizabeth <43956037+elaizae@users.noreply.github.com>
2021-05-16 21:42:01 +0200

    Update sketch.js

--- basic_website/sketch.js
+++ basic_website/sketch.js
@@ -183,8 +183,8 @@ function deviceMoved(){
   cx += dx*2;
   cy += dy*2; 
   // dont go off the screen
-  cx = constrain(cx, 0, width);
-  cy = constrain(cy, 0, height);
+  cx = constrain(cx, 0, cy, width);
+  cy = constrain(cy, 0, cx, height);
 
   fill(someColors1[randomColor]);
   Youcircle();
